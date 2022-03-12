49f24c393f1a3435c2d1b25f4e43c6e420f390bb
1406201502102017 <bekas.omsk@mail.ru>
2022-03-12 15:26:59 +0600

    JC lesson 5

--- src/main/java/ru/geekbrains/lessons/les5/CoWorkers.java
+++ src/main/java/ru/geekbrains/lessons/les5/CoWorkers.java
@@ -1,7 +1,5 @@
 package ru.geekbrains.lessons.les5;
 
-import java.util.Arrays;
-
 // 1. Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
 public class CoWorkers {
     private String name;
@@ -20,8 +18,9 @@ public class CoWorkers {
         this.age = age;
     }
 // 3. Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.
-    public static void infoEmployee (String name, String position, String email, String numberPhone, int salary, int age) {
+    public static Object infoEmployee(String name, String position, String email, String numberPhone, int salary, int age) {
         System.out.printf("Имя: %s\nДолжность: %s\nПочта: %s\nНомер телефона: %s\nЗарплата: %d\nВозраст: %d\n", name, position, email, numberPhone, salary, age);
+        return null;
     }
 // 4. Создать массив из 5 сотрудников.
 // 5. С помощью цикла вывести информацию только о сотрудниках старше 40 лет.
@@ -33,15 +32,13 @@ public class CoWorkers {
         staffArray[3] = new CoWorkers("Popov Mark", "CEO", "mPopov@yandex.ru", "861413332", 130000, 38);
         staffArray[4] = new CoWorkers("Sokolov Dima", "Manager", "dSokolov@list.ru", "892311594", 60000, 43);
         for (int i = 0; i < staffArray.length; i++) {
-            if(staffArray[i].age >= 40) {
-                System.out.println(staffArray[i].age);
+            if (staffArray[i].age >= 40) {
+                System.out.println(infoEmployee(staffArray[i].name, staffArray[i].position, staffArray[i].email, staffArray[i].numberPhone, staffArray[i].salary, staffArray[i].age) + "\n");
             }
         }
     }
 
     public static void main(String[] args) {
-        infoEmployee("Вася", "механик", "vasya@mail.ru", "+79065625147", 50000, 42);
-        System.out.println();
         staffArr();
     }
 }
